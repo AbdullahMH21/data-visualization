a952ec2aa08e5d456994065871fed6661bcd28c6
AbdullahAlhethely <abdullahmajed043@gmail.com>
2025-04-21 15:12:55 +0300

    committed4

--- visualize.py
+++ visualize.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("sample_data.cv")
+df = pd.read_csv("data-visualization/sample_data.csv")
 sns.histplot(data=df, x="Value", bins=20, kde=True)
 plt.figure(figsize=(10,4))
 sns.barplot(x="Category", y="Value", data=df)
